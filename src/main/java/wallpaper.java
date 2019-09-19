dec9126c3575eb19b0e7b993a8293317d5a1f678
ruslan <ruselsam89@gmail.com>
2019-09-19 22:21:37 +0300

    homework wallpaper added

---
+++ src/main/java/wallpaper.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class wallpaper {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter the length of the room: ");
+        double roomLength=scan.nextDouble();
+        System.out.print("Enter the height of the room: ");
+        double roomHeight=scan.nextDouble()+0.1; //0.1 bonding stock
+        System.out.print("Enter the width of the room: ");
+        double roomWidth=scan.nextDouble();
+        double roomWallArea=(roomLength*2+roomWidth*2)*roomHeight;
+        System.out.println("Room wall area: "+roomWallArea);
+        System.out.print("Enter the length of the wallpaper: ");
+        double wallpaperLength=scan.nextDouble();
+        System.out.print("Enter the width of the wallpaper: ");
+        double wallpaperWidth=scan.nextDouble();
+        double wallpaperArea=wallpaperLength*wallpaperWidth;
+        System.out.println("Wallpaper area: "+wallpaperArea);
+        double numbersOfRoll=roomWallArea/wallpaperArea;
+        int finalNumbersOfRoll=(int)numbersOfRoll;
+        if (roomWallArea%wallpaperArea>0){
+            finalNumbersOfRoll++;
+        }
+        System.out.println("Numbers of roll: "+finalNumbersOfRoll);
+
+    }
+}
