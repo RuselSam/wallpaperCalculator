b868e2a10620ac994dff07371eb135e08509e44e
ruslan <ruselsam89@gmail.com>
2019-09-20 21:29:37 +0300

    Rename variables,  added indent lines

--- src/main/java/wallpaper.java
+++ src/main/java/wallpaper.java
@@ -2,27 +2,29 @@ import java.util.Scanner;
 
 public class wallpaper {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter the length of the room: ");
-        double roomLength=scan.nextDouble();
+        double roomLength = scan.nextDouble();
         System.out.print("Enter the height of the room: ");
-        double roomHeight=scan.nextDouble()+0.1; //0.1 bonding stock
+        double roomHeight = scan.nextDouble();
+        double wallpaperWallHeightReserve = 0.1;
+        double roomHeightGluingMargin = roomHeight + wallpaperWallHeightReserve;
         System.out.print("Enter the width of the room: ");
-        double roomWidth=scan.nextDouble();
-        double roomWallArea=(roomLength*2+roomWidth*2)*roomHeight;
-        System.out.println("Room wall area: "+roomWallArea);
+        double roomWidth = scan.nextDouble();
+        double roomWallArea = (roomLength * 2 + roomWidth * 2) * roomHeightGluingMargin;
+        System.out.println("Room wall area: " + roomWallArea);
         System.out.print("Enter the length of the wallpaper: ");
-        double wallpaperLength=scan.nextDouble();
+        double wallpaperLength = scan.nextDouble();
         System.out.print("Enter the width of the wallpaper: ");
-        double wallpaperWidth=scan.nextDouble();
-        double wallpaperArea=wallpaperLength*wallpaperWidth;
-        System.out.println("Wallpaper area: "+wallpaperArea);
-        double numbersOfRoll=roomWallArea/wallpaperArea;
-        int finalNumbersOfRoll=(int)numbersOfRoll;
-        if (roomWallArea%wallpaperArea>0){
+        double wallpaperWidth = scan.nextDouble();
+        double wallpaperArea = wallpaperLength * wallpaperWidth;
+        System.out.println("Wallpaper area: " + wallpaperArea);
+        double numbersOfRoll = roomWallArea / wallpaperArea;
+        int finalNumbersOfRoll = (int) numbersOfRoll;
+        if (roomWallArea % wallpaperArea > 0) {
             finalNumbersOfRoll++;
         }
-        System.out.println("Numbers of roll: "+finalNumbersOfRoll);
+        System.out.println("Numbers of roll: " + finalNumbersOfRoll);
 
     }
 }
