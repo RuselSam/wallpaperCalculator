df0aba6d118dcad42e380ad5bf4ac83bd66aebf2
ruslan <ruselsam89@gmail.com>
2019-09-26 06:15:54 +0300

    fixed #2

--- src/main/java/Wallpaper.java
+++ src/main/java/Wallpaper.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class wallpaper {
+public class Wallpaper {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the length of the room: ");
