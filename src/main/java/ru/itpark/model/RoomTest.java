d70e220b98e0ecea95484d5fecd73fcab343f151
ruslan <ruselsam89@gmail.com>
2020-01-23 12:52:24 +0300

    Create branch dev & create test

---
+++ src/main/java/ru/itpark/model/RoomTest.java
@@ -0,0 +1,50 @@
+package ru.itpark.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @org.junit.jupiter.api.Test
+    void roomWallArea() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRoomHeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRoomLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRoomWidth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRoomHeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRoomLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRoomWidth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void canEqual() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
